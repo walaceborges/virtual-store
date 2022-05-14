453d1f3bfdbbc583189acb0f4d9372aa77c51346
Walace <walacewab@gmail.com>
2022-05-13 22:05:15 -0300

    feat: implementa o fetch de mudança de saldo do usuario

---
+++ client/src/components/CurrentBalance.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <span>{{ balance }}</span>
+    <span>{{ currency }}</span>
+  </button>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
--- client/src/services/admin.js
+++ client/src/services/admin.js
@@ -1,18 +1,18 @@
 const updateUserBalance = (userId, balance) => {
   try {
     const response = fetch("http://localhost:3001/user/balance", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `${localStorage.getItem("token")}`,
-    },
-    body: JSON.stringify({ userId, balance }),
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${localStorage.getItem("token")}`,
+        body: JSON.stringify({ userId, balance }),
+      },
+    });
 
     let data = response.json();
-    // console.log(data, "response");
-  });
+    console.log(data);
   } catch (error) {
-    
+    return false;
   }
 };
 
