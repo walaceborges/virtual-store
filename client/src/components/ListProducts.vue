ee288e9b60915a66064d11b4b55618feb251d874
Walace <walacewab@gmail.com>
2022-05-15 23:45:42 -0300

    fix: remove os comentarios da listProducts

--- client/src/components/ListProducts.vue
+++ client/src/components/ListProducts.vue
@@ -7,25 +7,19 @@
         <div v-for="product in products" :key="product.id">
           <div class="col mb-5">
             <div class="card h-100">
-              <!-- Sale badge-->
               <div
                 class="badge bg-dark text-white position-absolute"
                 style="top: 0.5rem; right: 0.5rem"
               >
                 Sale
               </div>
-              <!-- Product image-->
               <img class="card-img-top" v-bind:src="product.image" />
-              <!-- Product details-->
               <div class="card-body p-4">
                 <div class="text-center">
-                  <!-- Product name-->
                   <h5 class="fw-bolder">{{ product.name }}</h5>
-                  <!-- Product price-->
                   <span>{{ product.price }} Milhas</span>
                 </div>
               </div>
-              <!-- Product actions-->
               <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div class="text-center">
                   <button
